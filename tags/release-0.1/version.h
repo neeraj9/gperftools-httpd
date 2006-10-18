16d892d949de419f6f17322d4d33d5e2e20455b4
russcox <russcox@89af07de-1820-0410-a0a4-edb1ad95781f>
2006-10-18 17:18:08 +0000

    Tagging the 0.1 release.

---
+++ tags/release-0.1/version.h
@@ -0,0 +1,9 @@
+/* version.h - version defines for thttpd and libhttpd */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define SERVER_SOFTWARE "tchttpd/2.25b 17oct2006"
+#define SERVER_ADDRESS "http://swtch.com/tchttpd/"
+
+#endif /* _VERSION_H_ */
